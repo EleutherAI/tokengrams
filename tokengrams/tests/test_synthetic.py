b9b112fb6d2381c633a6ef40a022041d5ca20ab7
Nora Belrose <belrose.nora@gmail.com>
2024-03-20 04:57:02 +0000

    Remove Cow, get very simple GramIndex to work from Python

--- tokengrams/tests/test_synthetic.py
+++ tokengrams/tests/test_synthetic.py
@@ -1,5 +1,6 @@
 from tokengrams import GramIndex
 from hypothesis import given, strategies as st
+from itertools import pairwise
 
 
 @given(
@@ -14,3 +15,8 @@ def test_word_trie(tokens: list[int]):
     # Check unigram counts
     for t in tokens:
         assert index.count([t]) == tokens.count(t)
+
+    # Check bigram counts
+    bigrams = list(pairwise(tokens))
+    for b in bigrams:
+        assert index.count(list(b)) == bigrams.count(b)
