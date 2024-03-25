d39b9068f9540d4886363e52e9736815da526125
Nora Belrose <belrose.nora@gmail.com>
2024-03-25 05:28:19 +0000

    Fixed segfault in MemmapIndex

--- src/memmap_index.rs
+++ src/memmap_index.rs
@@ -1,8 +1,8 @@
-use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
-//use pyo3::exceptions::PyValueError;
+use indicatif::{ProgressBar, ProgressStyle};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use std::fs::{File, OpenOptions};
+use std::time::Instant;
 
 use crate::mmap_slice::{MmapSlice, MmapSliceMut};
 use crate::table::SuffixTable;
@@ -31,36 +31,48 @@ impl MemmapIndex {
     fn build(text_path: String, table_path: String) -> PyResult<Self> {
         // Memory map the text as read-only
         let text_mmap = MmapSlice::new(&File::open(&text_path)?)?;
-        let table_size = text_mmap.len() * std::mem::size_of::<u64>();
 
-        // Memory map the table as read-write and write indices 0..n to the table
+        // Create the table file
         let table_file = OpenOptions::new()
+            .create(true)
             .read(true)
             .write(true)
-            .create(true)
             .open(&table_path)?;
-        table_file.set_len(table_size as u64)?;
-        println!("Writing indices to disk...");
 
-        let pbar = ProgressBar::new(table_size as u64);
-        pbar.set_style(ProgressStyle::with_template("{human_pos}/{human_len}").unwrap());
+        // Allocate space on disk for the table
+        let table_size = text_mmap.len() * 8;
+        table_file.set_len(table_size as u64)?;
+
+        println!("Writing indices to disk...");
+        let start = Instant::now();
 
         let mut table_mmap = MmapSliceMut::<u64>::new(&table_file)?;
-        table_mmap
-            .par_iter_mut()
-            .progress_with(pbar)
-            .enumerate()
-            .for_each(|(i, index)| {
-                *index = i as u64;
-            });
+        table_mmap.iter_mut().enumerate().for_each(|(i, x)| *x = i as u64);
 
-        // Sort the indices by the suffixes they point to.
-        // The unstable algorithm is critical for avoiding out-of-memory errors, since it does
-        // not allocate any more memory than the input and output slices.
-        table_mmap.par_sort_unstable_by_key(|&i| &text_mmap[i as usize..]);
+        assert_eq!(table_mmap.len(), text_mmap.len());
+        println!("Time elapsed: {:?}", start.elapsed());
+        let start = Instant::now();
 
+        // TODO: Be even smarter about this? We may need to take into account the number of CPUs
+        // available as well. These magic numbers were tuned on a server with 48 physical cores.
+        // Empirically we start getting stack overflows between 5B and 10B tokens when using the
+        // default stack size of 2MB. We scale the stack size as log2(n) * 8MB to avoid this.
+        let scale = (text_mmap.len() as f64) / 5e9;     // 5B tokens
+        let stack_size = scale.log2().max(1.0) * 8e6;   // 8MB
+
+        rayon::ThreadPoolBuilder::new().stack_size(stack_size as usize).build().unwrap().install(|| {
+            // Sort the indices by the suffixes they point to.
+            // The unstable algorithm is critical for avoiding out-of-memory errors, since it does
+            // not allocate any more memory than the input and output slices.
+            println!("Sorting indices...");
+            table_mmap.par_sort_unstable_by_key(|&i| &text_mmap[i as usize..]);
+        });
+        println!("Time elapsed: {:?}", start.elapsed());
+
+        // Re-open the table as read-only
+        let table_mmap = MmapSlice::new(&table_file)?;
         Ok(MemmapIndex {
-            table: SuffixTable::from_parts(text_mmap, table_mmap.into_read_only()?),
+            table: SuffixTable::from_parts(text_mmap, table_mmap),
         })
     }
 
